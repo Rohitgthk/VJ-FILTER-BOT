9a3d4b1e583f98fde3118dcb8547c27430317a07
Rohitgthk <rk6848148607@gmail.com>
2025-03-04 15:25:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -136,7 +136,16 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 PORT = environ.get("PORT", "8080")
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION} https://t.me/Main_chenal")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION} 👇👇👇👇👇👇👇👇👇👇
+
+🌟 Movie Request Group 🌟
+        https://t.me/group_chaudhary
+
+🌟 OTT Update Channel 🌟
+👉     https://t.me/Main_chenal
+
+🅼🆁🅺
+Share And Support 😊👍")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
